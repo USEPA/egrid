ed24ebeb1e32811646b8d4a0861ecd8274a867a7
Sean Bock <bocks@aneprr-wxo.ad.abt.local>
2024-03-13 11:37:53 -0400

    Initial commit. Function to create summary table of dataframes, similar to what's produced by glimpse().

---
+++ scripts/functions/function_summary_table.R
@@ -0,0 +1,51 @@
+
+create_summary_table <- function(data) {
+
+    # Function to limit character values and add "..." if needed
+    limit_and_ellipsis <- function(vec, max_length) {
+      # Apply the function to each element in the vector
+      result <- sapply(vec, function(x) {
+        if (nchar(x) <= max_length) {
+          # If the length is within the limit, keep the original value
+          return(x)
+        } else {
+          # Otherwise, truncate and add "..."
+          return(paste0(substr(x, 1, max_length - 3), "..."))
+        }
+      })
+      return(result)
+    }
+    
+    first_five <- 
+      data %>%
+      dplyr::slice(1:5) %>%
+      dplyr::mutate(across(everything(), ~paste(.x, collapse = ", "))) %>%
+      slice(1) %>%
+      tidyr::pivot_longer(cols = everything(), values_to = "Values", names_to = "Columns") %>% 
+      mutate(Values = limit_and_ellipsis(Values, max_length = 40))
+    
+    col_types <- sapply(data, class)
+    
+    n_missing <- 
+      data %>% 
+      summarize(across(everything(), ~ sum(is.na(.x))))
+      
+    
+    nrows <- nrow(data)
+    
+    summary <- 
+      first_five %>% 
+      mutate(Class = col_types,
+             `N Missing` = as.numeric(n_missing[1,])) %>% 
+      relocate(Class, .after = Columns)
+    
+   gt_table <- 
+    summary %>% 
+      gt::gt() %>% 
+      gt::tab_caption(caption = glue::glue("N = {nrows}"))
+   
+   return(gt_table)
+    
+}
+
+
