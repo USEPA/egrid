efbfd91d04e7d020335efbc7f4c5521af01db860
Sean Bock <bocks@aneprr-wxo.ad.abt.local>
2024-03-14 18:17:10 -0400

    Initial commit to load and clean raw camd data.

---
+++ scripts/data_clean_camd.R
@@ -0,0 +1,34 @@
+
+library(dplyr)
+library(readxl)
+library(glue)
+library(stringr)
+library(janitor)
+library(purrr)
+
+egrid_year <- Sys.getenv("eGRID_year")
+
+
+# Reading raw camd files
+
+camd_facilities <- readr::read_rds("data/raw_data/camd/camd_facilities.RDS")
+camd_emissions <- readr::read_rds("data/raw_data/camd/camd_emissions.RDS")
+camd_emissions_ozone <- readr::read_rds("data/raw_data/camd/camd_emissions_ozone.RDS") %>% # adding "ozone" prefix to non-id cols
+  rename_with(.cols = -c("stateCode", "facilityName", "facilityId","unitId", "associatedStacks", "year"),
+              ~ paste0("ozone","_",.x))
+  
+
+# Combining files and standardizing variables names to match eia data
+
+
+camd_combined <- 
+  camd_facilities %>% 
+  left_join(camd_emissions) %>%
+  left_join(camd_emissions_ozone) %>%
+  janitor::clean_names() %>%
+  rename("plant_state" = state_code,
+         "plant_name" = facility_name,
+         "plant_id" = facility_id,
+         "primary_fuel_type" = primary_fuel_info,
+         "secondary_fuel_type" = secondary_fuel_type) %>%
+  arrange(plant_id, unit_id)
