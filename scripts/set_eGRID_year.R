c7d046942f1ebf4eb14a669604fb6cb542597d9b
ABT\BockS <sean_bock@abtassoc.com>
2024-04-08 20:49:19 -0400

    Changing default year to 2021 be inline with past QA on and comparisons to 2021 eGRID.

--- scripts/set_eGRID_year.R
+++ scripts/set_eGRID_year.R
@@ -1,7 +1,7 @@
 
 
 # read in text file containing year
-text <- readLines("eGRID_year.txt", warn = FALSE)
+text <- readr::read_lines("eGRID_year.txt")
 
 # extract only the year -- removing white spaces
 year <- stringr::str_trim(text[[5]])
