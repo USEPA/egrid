000c3a1381424b6fe2be0e1d9bdc2717f6529b76
Caroline Watson <Caroline_Watson@abtassoc.com>
2024-07-26 16:53:01 -0400

    Adding code to make heatinput become HeatInput in emission factors table

--- scripts/unit_file_create.R
+++ scripts/unit_file_create.R
@@ -285,8 +285,8 @@ nox_rates_ids <- # creating unique ids to filter out later
 
 #### NOx Ef   -----------------
 
-emission_factors <- read_csv("data/static_tables/emission_factors.csv") # load in emission_factors data
-
+emission_factors <- read_csv("data/static_tables/emission_factors.csv") %>% # load in emission_factors data
+  mutate(unit_flag = ifelse(unit_flag == "heatinput", "HeatInput", unit_flag))
 
 # Joining EFs df with camd ozone reporters to calculate non-ozone NOx mass
 
