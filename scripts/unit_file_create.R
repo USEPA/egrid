795d85fe7732f93d569956005bca78c8bd376e3d
Caroline Watson <Caroline_Watson@abtassoc.com>
2024-08-05 11:36:14 -0400

    Removing filter for SO2 emissions factors to place it later in the code when SO2 emissions are calculated.

--- scripts/unit_file_create.R
+++ scripts/unit_file_create.R
@@ -307,10 +307,8 @@ nox_rates_ids <- # creating unique ids to filter out later
 
 #### NOx Ef   -----------------
 
-emission_factors <- read_csv("data/static_tables/emission_factors.csv") %>% # load in emission_factors data
-  mutate(unit_flag = ifelse(unit_flag == "heatinput", "HeatInput", unit_flag)) %>%
-  filter(primary_fuel_type != "OTH" |
-           so2_ef != 2.8)
+emission_factors <- read_csv("data/static_tables/emission_factors.csv") # load in emission_factors data
+
 
 # Joining EFs df with camd ozone reporters to calculate non-ozone NOx mass
 
