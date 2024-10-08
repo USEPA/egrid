23732174b0dffe73170c50958b0b627c4872cdca
ABT\zhangm <madeline.zhang@abtglobal.com>
2024-10-08 16:37:03 -0400

    Creating feature that generates a formatted summary table of all states that replicates VBA_GGL sheet, also added in static tables: "NERC region and interconnect" and "State and Interconnection"

--- scripts/grid_gross_loss_create.R
+++ scripts/grid_gross_loss_create.R
@@ -44,18 +44,16 @@ eia_923_generation_and_fuel <- eia_923$generation_and_fuel_data
 ## Function to download EIA files from website (adapted from function: "download_eia_files" )-------
 ## These urls are different depending whether or not they're the most recent data. If not most recent year, "archive" is in the url
 
-
-
-download_eia_ggl <- function() {
+download_eia_ggl <- function(year) {
   
   #' download_eia_ggl
   #' 
   #' Function to download data from EIA website (https://www.eia.gov/electricity/) for all states. Each states' data is stored in a Excel file on the EIA website. This function extracts all of the tables and combines it into one and creates a summary table.
   
+  #' @year Year desired for formatted table
   #' @return Unzipped Excel file
   #' @examples
-  #' download_eia_files("923") # Download a single form's data
-  #' purrr::map(c("923","860","861"), ~ download_eia_files(.x)) # Download and unzip forms 923, 860, and 861
+  #' download_eia_files("2022") # Downloads all data and summarizes all states for year 2022
   
   state <- gsub(" ", "", tolower(datasets::state.name))
   state_abbr <- tolower(datasets::state.abb)
@@ -104,6 +102,8 @@ download_eia_ggl <- function() {
   ggl_wb <- createWorkbook()
   ggl_summary_data <- list()
   
+  summary_sheet_name <- glue::glue("FormattedTable_{year}")
+  addWorksheet(ggl_wb, summary_sheet_name)
   
   # loop for all of the states and files
   for (i in 1:length(state_abbr)){
@@ -126,8 +126,85 @@ download_eia_ggl <- function() {
     addWorksheet(ggl_wb, sheetName = toupper(abbr))
     writeData(ggl_wb, sheet = toupper(abbr), ggl_data[[i]])
     
-  }
+    # clean up select table for easier data extraction
+    new_header <- select_table[3,]
+    colnames(select_table) <- new_header # changes header to be by year
+    colnames(select_table) <- gsub("Year", "", colnames(select_table)) # removes extra spaces in header for easy extraction
+    colnames(select_table) <- gsub("\r\n", "", colnames(select_table))
     
+    ## extracting desired rows for the indicated year
+    direct_use <- select_table %>%
+      filter(Category == "Direct use") %>%
+      pull(year) %>%
+      as.numeric() 
+    
+    estimated_losses <- select_table %>%
+      filter(Category == "Estimated losses") %>%
+      pull(year) %>%
+      as.numeric() 
+    
+    total_disp <- select_table %>%
+      filter(Category == "Total disposition") %>%
+      pull(year) %>%
+      as.numeric() 
+    
+    net_interstate_i <- select_table %>%
+      filter(Category == "Net interstate imports") %>%
+      pull(year) %>%
+      as.numeric() 
+    
+    net_interstate_e <- select_table %>%
+      filter(Category == "Net interstate exports") %>%
+      pull(year) %>%
+      as.numeric() 
+    
+    total_disp_sub_e <- (total_disp - net_interstate_e)
+    
+    # compile all variables into a single row
+    ggl_summary_data[[i]] <- c(toupper(abbr), 
+                    tools::toTitleCase(name), 
+                    direct_use, estimated_losses, total_disp, net_interstate_i, net_interstate_e, total_disp_sub_e)
+    
+    
+  }
+  
+    
+  # adding formatted table
+  
+  formatted_table <- do.call(rbind, ggl_summary_data)
+  formatted_table <- as.data.frame(formatted_table)
+  colnames(formatted_table) <- c("State Postal Code", 
+                      "State", 
+                      "Direct Use", 
+                      "Estimated Losses", 
+                      "Total Disposition", 
+                      "Net Interstate Imports",
+                      "Net Interstate Exports",
+                      "Total Disposition-Exports")
+  
+  formatted_table <- formatted_table %>%
+    mutate(across(c("Direct Use", 
+                    "Estimated Losses", 
+                    "Total Disposition", 
+                    "Net Interstate Imports",
+                    "Net Interstate Exports",
+                    "Total Disposition-Exports"), ~as.numeric(.)))
+  
+  # create total_row for US using sums of all states
+  total_row <- c("US", "United States",
+             sum(formatted_table$`Direct Use`),
+             sum(formatted_table$`Estimated Losses`),
+             sum(formatted_table$`Total Disposition`),
+             sum(formatted_table$`Net Interstate Imports`),
+             sum(formatted_table$`Net Interstate Exports`),
+             sum(formatted_table$`Total Disposition-Exports`))
+  
+  # add row to FormattedTable
+  formatted_table <- rbind(formatted_table, total_row)
+  
+  # write FormattedTable to Excel file
+  writeData(ggl_wb, sheet = summary_sheet_name, formatted_table)
+  
   
   ggl_file <- glue::glue("{new_folder2}/R_ggl.xlsx")
   saveWorkbook(ggl_wb, ggl_file, overwrite = TRUE)
@@ -142,25 +219,89 @@ download_eia_ggl <- function() {
   
 }
 
-download_eia_ggl()
+download_eia_ggl("2022")
 
-## area to develop new parts of the function :-3
-# new_folder <- glue::glue("data/raw_data/eia_ggl")
-# dest_file1 <- glue::glue("{new_folder}/al.xlsx")
-# select_table1 <- read_excel(dest_file1, sheet = 11)
+## area to develop new parts of the function :-3 ------
+
+# new_header <- select_table1[3,]
+# colnames(select_table1) <- new_header # changes header to be by year
+# colnames(select_table1) <- gsub("Year", "", colnames(select_table1)) # removes extra spaces in header for easy extraction
+# colnames(select_table1) <- gsub("\r\n", "", colnames(select_table1))
 # 
-# ggl_data <- list()
-# ggl_data[[1]] <- select_table1
+# ## extracting desired rows for the indicated year
+# direct_use1 <- select_table1 %>%
+#   filter(Category == "Direct use") %>%
+#   pull("2022") %>%
+#   as.numeric() 
 # 
-# wb <- createWorkbook()
-# addWorksheet(wb, sheetName = 'AL')
-# writeData(wb, sheet = 'AL', ggl_data[[1]])
+# estimated_losses1 <- select_table1 %>%
+#   filter(Category == "Estimated losses") %>%
+#   pull("2022") %>%
+#   as.numeric() 
 # 
-# ggl_file <- glue::glue("{new_folder}/R_ggl.xlsx")
-# saveWorkbook(wb, ggl_file, overwrite = TRUE)
+# total_disp1 <- select_table1 %>%
+#   filter(Category == "Total disposition") %>%
+#   pull("2022") %>%
+#   as.numeric() 
+# 
+# net_interstate_i1 <- select_table1 %>%
+#   filter(Category == "Net interstate imports") %>%
+#   pull("2022") %>%
+#   as.numeric() 
+# 
+# net_interstate_e1 <- select_table1 %>%
+#   filter(Category == "Net interstate exports") %>%
+#   pull("2022") %>%
+#   as.numeric() 
+# 
+# total_disp_sub_e1 <- (total_disp1 - net_interstate_e1)
+# 
+# 
+# # compile all variables into a single row
+# test <- list()
+# test[[1]] <- c("AL", "Alabama", direct_use1, estimated_losses1, total_disp1, net_interstate_i1, net_interstate_e1, total_disp_sub_e1)  
+# test[[2]] <- c("AL", "Alabama", direct_use1, estimated_losses1, total_disp1, net_interstate_i1, net_interstate_e1, total_disp_sub_e1)    
+# 
+# formattest <- do.call(rbind, test)
+# formattest <- as.data.frame(formattest)
+# colnames(formattest) <- c("State Postal Code", 
+#                                "State", 
+#                                "Direct Use", 
+#                                "Estimated Losses", 
+#                                "Total Disposition", 
+#                                "Net Interstate Imports",
+#                                "Net Interstate Exports",
+#                                "Total Disposition-Exports")
+# 
+# formattest <- formattest %>%
+#   mutate(across(c("Direct Use", 
+#                   "Estimated Losses", 
+#                   "Total Disposition", 
+#                   "Net Interstate Imports",
+#                   "Net Interstate Exports",
+#                   "Total Disposition-Exports"), ~as.numeric(.)))
+# 
+# test2 <- c("US", "United States",
+#            sum(formattest$`Direct Use`),
+#            sum(formattest$`Estimated Losses`),
+#            sum(formattest$`Total Disposition`),
+#            sum(formattest$`Net Interstate Imports`),
+#            sum(formattest$`Net Interstate Exports`),
+#            sum(formattest$`Total Disposition-Exports`))
+# 
+# formattest <- rbind(formattest, test2)
+# 
+# str(formattest$`Direct Use`)
 
 
-## 01: Create GGL table at state level for non duplicate states
+
+## load in datasets ----
+R_ggl <- read_xlsx("data/clean_data/eia_ggl/R_ggl.xlsx")
+state_interconnect <- read_csv("data/static_tables/state_and_interconnection.csv")
+nerc_interconnect <- read_xlsx("data/static_tables/nerc_region_and_interconnect.xlsx")
+
+
+## 01: Create GGL table at state level for non duplicate states -----
 
 # select - state and interconnection, state postal code, state 
 
