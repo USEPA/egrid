838b0b2ba58720394b0c8166519bf986de5c4c42
ABT\russelle <Emma.Russell@abtglobal.com>
2024-11-11 12:43:37 -0500

    Creating new metric conversion file and adding outline

---
+++ scripts/metric_conversion.R
@@ -0,0 +1,38 @@
+## -------------------------------
+##
+## Metric conversion file
+## 
+## Purpose: 
+## 
+## This file creates new .RDS output files with unit conversions to metric
+## when necessary utilizing conversion table with conversion rates
+##
+## Authors:  
+##      Emma Russell, Abt Global
+##
+## -------------------------------
+
+# Load libraries
+
+# Load in conversion table
+
+# Add new variables with metric conversions
+
+# unit file
+
+# generator file
+
+# plant file
+
+# state file
+
+# balancing authority file
+
+# subregion file
+
+# nerc region file
+
+# US file
+
+# grid gross loss
+
