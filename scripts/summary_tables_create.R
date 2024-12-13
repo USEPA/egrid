24b87efb01e3cf9b4b9e1699891bff2a52cf9265
ABT\russelle <Emma.Russell@abtglobal.com>
2024-12-13 17:36:51 -0500

    Selecting desired data for summary tables

--- scripts/summary_tables_create.R
+++ scripts/summary_tables_create.R
@@ -45,6 +45,66 @@ for (file in (r_files)){
   assign(str_remove(file, ".RDS"), read_rds(paste0(rdata_dir, file)))
 }
 
+# associate larger regions to subregions, assign ggl
+#subregion_ggl <-
+
+# add US rows
+  
 # Pull desired data
 
-# Format to excel sheet
+# table 1
+emission_types <- c(
+  "co2",
+  "ch4",
+  "n2o",
+  "co2e",
+  "nox",
+  "nox_oz",
+  "so2")
+
+subregion_output_emissions <-
+  subregion_aggregation %>%
+  select(subregion, subregion_name, any_of(paste0("subregion_", emission_types, "_output_rate")), 
+                any_of(paste0("subregion_", emission_types, "_output_rate_nonbaseload"))) %>%
+  arrange(subregion) #%>%
+  #rbind(subregoin_ggl)
+
+glimpse(subregion_output_emissions)
+
+  
+# table 2
+resource_type <- c(
+  "coal",
+  "oil",
+  "gas",
+  "other_ff",
+  "nuclear",
+  "hydro",
+  "biomass",
+  "wind",
+  "solar",
+  "geothermal",
+  "other")
+  
+# subregion resource mix
+subregion_resource_mix <-
+  subregion_aggregation %>%
+  select(subregion, subregion_name, subregion_nameplate_capacity, subregion_generation_ann, 
+         any_of(paste0("subregion_ann_resource_mix_", resource_type)))
+glimpse(subregion_resource_mix)
+  
+# table 3
+# state output emission rates
+state_output_emissions <-
+  state_aggregation %>%
+  select(state, any_of(paste0("state_", emission_types, "_output_rate")))
+glimpse(state_output_emissions)        
+
+# table 4
+# state resource mix
+state_resource_mix <-
+  state_aggregation %>%
+  select(state, state_nameplate_capacity, state_generation_ann, 
+         any_of(paste0("state_ann_resource_mix_", resource_type)))
+glimpse(state_resource_mix)
+
