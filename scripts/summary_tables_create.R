ff1dd6732bbb08ac8c4e501392858e63f9a6a14f
ABT\russelle <Emma.Russell@abtglobal.com>
2024-12-16 11:07:47 -0500

    Adding crosswalk for subregions to interconnect regions, assigning ggl to subregions, finalizing datasets for each summary table

--- scripts/summary_tables_create.R
+++ scripts/summary_tables_create.R
@@ -18,7 +18,6 @@ library(dplyr)
 library(glue)
 library(readr)
 library(readxl)
-#library(xlsx)
 library(stringr)
 
 # Define eGRID year ------------------
@@ -38,21 +37,26 @@ if (exists("params")) {
 
 # create a list of files in R directory
 rdata_dir <- glue::glue("data/outputs/{params$eGRID_year}/")
-r_files <- list.files(path = rdata_dir, pattern = "*[^c].RDS")
+r_files <- list("state_aggregation.RDS", "subregion_aggregation.RDS", 
+                "grid_gross_loss.RDS", "us_aggregation.RDS")
 
 # import files in list
 for (file in (r_files)){
   assign(str_remove(file, ".RDS"), read_rds(paste0(rdata_dir, file)))
 }
 
-# associate larger regions to subregions, assign ggl
-#subregion_ggl <-
+# Compile and format subregion output emissions rates for TABLE 1   ------------------
 
-# add US rows
-  
-# Pull desired data
+# read in crosswalk assigning subregions to interconnect regions
+xwalk_subregion_ggl <- 
+  read.csv("data/static_tables/xwalk_subregion_interconnect.csv") %>%
+  janitor::clean_names() %>%
+  rename(subregion = subregion_code) %>%
+  # join grid gross loss data by interconnect assignment
+  left_join(grid_gross_loss %>%
+              select(interconnect, ggl), by = "interconnect")
 
-# table 1
+# list emission types wanted in summary tables
 emission_types <- c(
   "co2",
   "ch4",
@@ -62,26 +66,29 @@ emission_types <- c(
   "nox_oz",
   "so2")
 
-# create dataset with US emissions data
+# US output aemissions data
 us_output_emissions <-
   us_aggregation %>%
   select(any_of(paste0("us_", emission_types, "_output_rate")), 
          any_of(paste0("us_", emission_types, "_output_rate_nonbaseload"))) %>%
   rename_with(~str_c(str_remove(.,"us_")))
-glimpse(us_reduced)
 
-# create dataset with subregion emissions
+# subregion output emissions
 subregion_output_emissions <-
   subregion_aggregation %>%
   select(subregion, subregion_name, any_of(paste0("subregion_", emission_types, "_output_rate")), 
-                any_of(paste0("subregion_", emission_types, "_output_rate_nonbaseload"))) %>%
+         any_of(paste0("subregion_", emission_types, "_output_rate_nonbaseload"))) %>%
   rename_with(~str_c(str_remove(.,"subregion_"))) %>%
   # add US data to bottom row
-  bind_rows(us_output_emissions)
-glimpse(subregion_output_emissions)
+  bind_rows(us_output_emissions) %>%
+  # add grid gross loss as column
+  left_join(xwalk_subregion_ggl, by= "subregion") %>%
+  # remove interconnect data
+  select(!interconnect)
 
+# Compile and format subregion resource mix data for TABLE 2 ------------------
   
-# table 2
+# list resource types wanted in summary tables
 resource_type <- c(
   "coal",
   "oil",
@@ -95,13 +102,12 @@ resource_type <- c(
   "geothermal",
   "other")
 
-# create dataset with US emissions data
+# US emissions data
 us_resource_mix <-
   us_aggregation %>%
   select(us_nameplate_capacity, us_generation_ann, any_of(paste0("us_ann_resource_mix_", resource_type))) %>%
   rename_with(~str_c(str_remove(.,"us_")))
-glimpse(us_resource_mix)
-  
+
 # subregion resource mix
 subregion_resource_mix <-
   subregion_aggregation %>%
@@ -110,20 +116,30 @@ subregion_resource_mix <-
   rename_with(~str_c(str_remove(.,"subregion_"))) %>%
   # add US data to bottom row
   bind_rows(us_resource_mix)  
-glimpse(subregion_resource_mix)
-  
-# table 3
+
+
+# Compile and format state output emission rates for TABLE 3 -------------------
+
 # state output emission rates
 state_output_emissions <-
   state_aggregation %>%
-  select(state, any_of(paste0("state_", emission_types, "_output_rate")))
-glimpse(state_output_emissions)        
+  select(state, any_of(paste0("state_", emission_types, "_output_rate"))) %>%
+  rename_with(~str_c(str_remove(.,"state_"))) %>%
+  # add US data to bottom row
+  bind_rows(us_output_emissions %>% select(!contains("nonbaseload")))  
 
-# table 4
+# Compile and format state resource mix data for TABLE 4  ----------------------
+
 # state resource mix
 state_resource_mix <-
   state_aggregation %>%
   select(state, state_nameplate_capacity, state_generation_ann, 
-         any_of(paste0("state_ann_resource_mix_", resource_type)))
-glimpse(state_resource_mix)
+         any_of(paste0("state_ann_resource_mix_", resource_type))) %>%
+  rename_with(~str_c(str_remove(.,"state_"))) %>%
+  # add US data to bottom row
+  bind_rows(us_resource_mix)  
+
+# Format as excel sheet ---------------------------
+
+# Save -------------------------------
 
