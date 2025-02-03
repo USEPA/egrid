d7d2f5078c59fc4103c9032016514e8f158146ac
ABT\russelle <Emma.Russell@abtglobal.com>
2025-02-03 17:06:49 -0500

    Updating inclusion of estimated emissions in unit file to update correct rows

--- scripts/pm_file_create.R
+++ scripts/pm_file_create.R
@@ -45,7 +45,7 @@ if(file.exists(glue::glue("data/clean_data/eia/{params$eGRID_year}/eia_923_clean
 
 ## NEI PM data
 if(file.exists(glue::glue("data/raw_data/nei/{params$eGRID_year}/emis_sum_unit_25291.csv"))) { 
-  pm_data_nei <- read_csv(glue::glue("data/raw_data/nei/{params$eGRID_year}/emis_sum_unit_25291.csv")) %>%
+  pm25_raw_nei <- read_csv(glue::glue("data/raw_data/nei/{params$eGRID_year}/emis_sum_unit_25291.csv")) %>%
     janitor::clean_names()
 } else { 
   stop("emis_sum_unit_25291.csv does not exist.")}
@@ -59,91 +59,115 @@ nei_eia_xwalk <- read_csv("data/static_tables/fac_conf_alt_id_25292.csv") %>% #,
 ## eGRID production model data
 unit_file <- read_xlsx(glue::glue("data/outputs/{params$eGRID_year}/unit_file_2021_access.xlsx")) %>%
   janitor::clean_names()
-  # both have 24597 observations
-  # both have 1819 missing htian values
+  # both have 24597 observations, 1819 missing htian values
+
 #unit_file <- read_rds(glue::glue("data/outputs/{params$eGRID_year}/unit_file.RDS"))
 
 # Calculate PM data -------------
 ## 1) Direct Match - "NEI/EIA" --------------
-  # access queries 000-01b
-  # match NEI (EIS codes) to eGRID (ORISPL codes)
-  # using crosswalk (QAQPS)
+  ## Queries 000, 00, 01a, 01b, 2
+  ## Match NEI (EIS codes) to eGRID (ORISPL codes) with crosswalk (QAQPS)
+  ## Calculate the sum of pm emissions for each facility ID and boiler ID combination
 
-# join pm data with matching crosswalk data (query 000)
-  # matching by facility and unit IDs
-  # left join keeps all pm records and any matching crosswalk records
-  # 3918 matching records from crosswalk
-pm_unit_match <- # (matching 356369 observations)
-  pm_data_nei %>%
+pm25_direct_match <-
+  pm25_raw_nei %>%
   left_join(nei_eia_xwalk, by = join_by(eis_facility_id, eis_unit_id)) %>%
   select(oris_facility_code = left_side_of_alternate_id_to_become_oris_facility_code, oris_boiler_id = right_side_of_alternatve_id_to_become_oris_boiler_id, 
          pm25 = total_emissions, state = state.y,
-         eis_facility_id, eis_unit_id, agency_unit_id)
-
-# calculate sum of pm emissions for each facility ID and boiler ID combination (query 00)
- # 3907 observations of summed pm25 (all not NA)
-pm_sum_facility_boiler <-
-  pm_unit_match %>%
+         eis_facility_id, eis_unit_id, agency_unit_id) %>%
   group_by(oris_facility_code, oris_boiler_id) %>%
-  summarise(sum_pm25 = sum(pm25))
+  summarise(sum_pm25 = sum(pm25)) %>%
+  ungroup() #%>%
+  # right_join(unit_file, by = join_by(oris_boiler_id == unitid, oris_facility_code == orispl)) %>%
+  # rename(pm25 = sum_pm25, unitid = oris_boiler_id, orispl = oris_facility_code) %>%
+  # mutate(pm25_source = if_else(is.na(pm25), NA, "EPA/NEI"), eia_pm_control_efficiency = NA, botfirty = if_else(botfirty == "", NA, botfirty))
 
 # match unit file units to pm25 sums (query 01,01a,01b,2)
   # join by unit id and facility id
   # 3582 matches, now 3582 rows have pm data (same as access)
-unit_pm_emissions <-
+unit_pm25_emissions <-
   unit_file %>%
-  left_join(pm_sum_facility_boiler, by = join_by(unitid == oris_boiler_id, orispl == oris_facility_code)) %>%
+  left_join(pm25_direct_match, by = join_by(unitid == oris_boiler_id, orispl == oris_facility_code)) %>%
   rename(pm25 = sum_pm25) %>%
   mutate(pm25_source = if_else(is.na(pm25), NA, "EPA/NEI"), eia_pm_control_efficiency = NA, botfirty = if_else(botfirty == "", NA, botfirty))
 
 ## 2) Match by fuel type, unit firing type, and prime mover - "NEI avg EF - PM, fuel type, firing type" ----------
-  ## Group units by fuel type, firing type, and prime mover
+  ## Queries 03a, 03b, 03c
+  ## Group EPA/NEI-matched units (3582) by fuel type, firing type, and prime mover (59 combinations)
   ## Calculate the heat input and pm2.5 sums for groups
-  ## Calculate emission factors from sum
+  ## Calculate emission factors from sum (EF = pm2.5 sum / heat input sum)
   ## Multiply individual heat inputs by emission factors to estimate pm2.5
-  ## Add estimates to dataset
 
-# calculate emission factors for fuel type, unit firing type, and prime mover combinations (query 03a,03b)
-  # select EPA/NEI data (3582)
-  # calculate the sum of heat input and sum of pm2.5
-  # for all prime mover, firing type, and fuel type combinations (59 combinations)
-  # emission factors = pm2.5 / heat input
-emission_factors_fuel_pm_firing <-
-  unit_pm_emissions %>%
-  filter(pm25_source == "EPA/NEI") %>%
-  group_by(prmvr, botfirty, fuelu1, pm25_source) %>%
+pm25_fuel_pmover_firing <-
+  unit_pm25_emissions %>%
+  filter(pm25_source == "EPA/NEI") %>% # filter to those that have a match with NEI
+  group_by(prmvr, botfirty, fuelu1, pm25_source) %>% # group by prime mover, firing, fuel type
   summarise(sum_heat_input = sum(htian), sum_pm25 = sum(pm25)) %>%
-  relocate(pm25_source, .after = sum_pm25) %>%
-  mutate(emission_factors = sum_pm25 / sum_heat_input)
+  mutate(emission_factors = sum_pm25 / sum_heat_input) %>% # calculate emission factor
+  inner_join(unit_pm25_emissions, by = join_by(prmvr, botfirty, fuelu1)) %>%
+  mutate(pm25 = emission_factors * htian, pm25_source = "NEI avg EF - PM, fuel type, firing type") %>%
+  ungroup() %>%
+  select(orispl, unitid, prmvr, pm25, pm25_source)
 
-# calculate pm2.5 emissions estimate for grouping (query 03c)
-  #multiply heat inputs for each unit by estimated emission factors (4353)
-pm25_fuel_pm_firing <-
-  unit_pm_emissions %>%
-  inner_join(emission_factors_fuel_pm_firing, by = join_by(prmvr, botfirty, fuelu1)) %>%
-  mutate(pm25_from_nei_avg = emission_factors * htian) %>%
-  select(orispl, unitid, pm25_from_nei_avg, pm25_source_estimate = pm25_source.y)
+## 3) Match by fuel type and prime mover - "NEI avg EF - PM, fuel type" ----------
+  ## Queries 04a, 04b, 04c
+  ## Group EPA/NEI-matched units (3582) by fuel type and prime mover (25 combinations)
+  ## Calculate the heat input and pm2.5 sums for groups
+  ## Calculate emission factors from sum (EF = pm2.5 sum / heat input sum)
+  ## Multiply individual heat inputs by emission factors to estimate pm2.5
 
-# add pm estimates to unit emissions data (query 03d)
-  # identify units that don't already have a direct match estimate
-  # 4357 estimates, can fill 775 that don't already have an estimate
-unit_pm_emissions_updated <-
-  unit_pm_emissions %>%
-  left_join(pm25_fuel_pm_firing, by = join_by(unitid, orispl)) %>%
-  mutate(pm25_source = if_else(is.na(pm25) & !is.na(pm25_source_estimate), "NEI avg EF - PM, fuel type, firing type", pm25_source)) %>%
-  mutate(pm25 = if_else(is.na(pm25), pm25_from_nei_avg, pm25)) %>%
-  select(-pm25_source_estimate, -pm25_from_nei_avg) #%>%
-  # count(pm25_source)
+pm25_fuel_pmover <-
+  unit_pm25_emissions %>%
+  filter(pm25_source == "EPA/NEI") %>%
+  group_by(prmvr, fuelu1, pm25_source) %>%
+  summarise(sum_heat_input = sum(htian), sum_pm25 = sum(pm25)) %>%
+  mutate(emission_factors = sum_pm25 / sum_heat_input) %>%
+  inner_join(unit_pm25_emissions, by = join_by(prmvr, fuelu1)) %>%
+  mutate(pm25 = emission_factors * htian, pm25_source = "NEI avg EF - PM fuel type") %>%
+  ungroup() %>%
+  select(orispl, unitid, prmvr, pm25, pm25_source)
 
-#************************************************************
 
-# 3) Match by fuel type and prime mover
-#4a-4d
-#pm25src = "NEI avg EF - PM, fuel type"
+## 4) Use a similar fuel type ---------
+  ## pm25src = "Estimated using an emission factor"
 
-# 4) Use a similar fuel type
-# 5
-#pm25src = "Estimated using an emission factor"
 
-# 5) Use a similar fuel type
+## 5) Add pm2.5 emission estimates to unit data -------------
+  ## Queries 03d, 04d, 
 
+unit_pm25_emissions_updated <-
+  unit_pm25_emissions %>%
+  rows_patch(pm25_fuel_pmover_firing, by = c("unitid", "orispl")) %>%
+  rows_patch(pm25_fuel_pmover, by = c("unitid", "orispl"))
+
+# unit_pm25_emissions_2 <-
+#   unit_pm_emissions %>%
+#   rows_patch(pm25_fuel_pmover_firing, by = c("unitid", "orispl"))
+# 
+# count <-
+#   unit_pm25_emissions_2 %>%
+#   count(pm25_source)
+# 
+# unit_pm_emissions_3 <-
+#   unit_pm_emissions_2 %>%
+#   rows_patch(pm25_fuel_pmover, by = c("unitid", "orispl"))
+# 
+# count <-
+#   unit_pm_emissions_3 %>%
+#   count(pm25_source)
+
+
+###---------- NOTES --------------###
+
+## we have duplicate matches for orispl 2707 and unitid 1-4
+  
+  # for the values that are still NA after an estimate, should I then replace them if there is an estimate?
+
+
+## in access the second estimate went from 775 -> 668 (-107)
+## the third estimate is 1681 (if we remove the duplicates, we should expect 1574 to be in the third category)
+## right now we have 1594 in the third category although we initially have in the inner join 5951 instead of 5947 (an extra 4)
+
+## need to check on the state of this - it seems to override ones that are already listed as the NEI avg from the three categories - need a better system to determine when to rewrite data! 
+
+###-----------------------------------####
