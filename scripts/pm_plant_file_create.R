f844ac898a49227579f6f15a29698852e9163a77
ABT\russelle <Emma.Russell@abtglobal.com>
2025-02-06 23:59:23 -0500

    Creating pm2.5 plant file script

---
+++ scripts/pm_plant_file_create.R
@@ -0,0 +1,13 @@
+## Purpose: 
+## 
+## This file creates the PM2.5 plant file for eGRID. 
+## This file includes PM2.5 emission data, either calculated
+## or estimated for the plants of the specified eGRID year
+## 
+## The method of PM2.5 calculations are listed within pm25_source
+##
+## Additional notes
+##
+##      Emma Russell, Abt Global
+##
+## -------------------------------
