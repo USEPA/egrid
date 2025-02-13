f68077594796b4a45d7e424bc346eb821d7d9165
ABT\russelle <Emma.Russell@abtglobal.com>
2025-02-13 16:58:12 -0500

    Summing PM2.5 emissions data to plant level and joining with plant file data

--- scripts/pm_plant_file_create.R
+++ scripts/pm_plant_file_create.R
@@ -11,3 +11,59 @@
 ##      Emma Russell, Abt Global
 ##
 ## -------------------------------
+
+# Load Libraries ---------
+library(dplyr)
+library(readr)
+library(readxl)
+
+
+# Define eGRID year parameter ----------------
+
+# define parameter year if no one is currently assigned using prompted user input
+if (exists("params")) {
+  if ("eGRID_year" %in% names(params)) { # if params() and params$eGRID_year exist, do not re-define
+    print("eGRID year parameter is already defined.")
+  } else { # if params() is defined, but eGRID_year is not, define it here
+    params$eGRID_year <- readline(prompt = "Input eGRID_year: ")
+    params$eGRID_year <- as.character(params$eGRID_year)
+  }
+} else { # if params() and eGRID_year are not defined, define them here
+  params <- list()
+  params$eGRID_year <- readline(prompt = "Input eGRID_year: ")
+  params$eGRID_year <- as.character(params$eGRID_year)
+}
+
+# Load necessary data --------------------
+## PM2.5 unit file
+if(file.exists(glue::glue("data/outputs/{params$eGRID_year}/pm_unit_file.RDS"))) {
+  pm_unit_file <- read_rds(glue::glue("data/outputs/{params$eGRID_year}/pm_unit_file.RDS"))
+} else {
+  stop("pm_unit_file.RDS does not exist. Run pm_unit_file_create.R to obtain.")}
+
+## eGRID production model data - plant file
+plant_file <- read_csv(glue::glue("data/outputs/{params$eGRID_year}/plant_file_2021_access.csv"), col_types = "ccccccccccicddddddddccccccccccc") %>%
+  janitor::clean_names()
+
+
+# Sum PM2.5 unit data by plant id ---------
+plant_agg <-
+  pm_unit_file %>%
+  group_by(orispl) %>%
+  summarise(pm25 = sum(pm25))
+
+# Add PM2.5 data to plant file ---------
+plant_emissions <-
+  plant_file %>%
+  inner_join(plant_agg, by = join_by(orispl)) %>%
+  # set plant electric allocation factors to 1 if NaN 
+  mutate(elcalloc = if_else(is.na(elcalloc), 1, elcalloc),
+         # calculate annual pm2.5 emissions
+         plpm25an = pm25 * elcalloc,
+         # calculate total output emission rate
+         plpm25rta= plpm25an * 2000 / plngenan,
+         # calculate total input emission rate
+         plpm25ra = plpm25an * 2000 / plhtian,
+         #  rename unadjusted annual pm2.5 emissions
+         unpm25 = pm25) %>%
+  select(pstatabb, pname, orispl, srname, subrgn, plprmfl, namepcap, elcalloc, plhtian, plngenan, plpm25an, plpm25rta, plpm25ra, unhti, unpm25)
