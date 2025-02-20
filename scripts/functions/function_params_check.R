121674d53c504fe7f8b6cf1b0a7122fef87bce55
ABT\zhangm <madeline.zhang@abtglobal.com>
2025-02-20 11:47:04 -0500

    Rename function_cols_to_add to function_temporal_res_cols, split function_params_check

--- scripts/functions/function_params_check.R
+++ scripts/functions/function_params_check.R
@@ -62,68 +62,6 @@ params_check <- function() {
     return(params_check()) # restart function for new inputs
   }
   
-  # Specify grouping columns based on temporal_res parameter
-  temporal_res_cols_all <- 
-    list("annual"  = c("year"), 
-         "monthly" = c("year", "month"), 
-         "daily"   = c("year", "month", "day"), 
-         "hourly"  = c("year", "month", "day", "hour"))
-  
-  # Save column names 
-  params$temporal_res_cols <- unlist(temporal_res_cols_all[params$temporal_res], use.names = FALSE)
-  
-  
-  # Create dataframe that contains all date and time combinations. 
-  if (params$temporal_res == "annual") { 
-    df <- data.frame(year = params$eGRID_year)}
-  if (params$temporal_res == "monthly") { 
-    df <- data.frame(year = params$eGRID_year, 
-                     month = c(1:12))}
-  if (params$temporal_res == "daily") {
-    if ((as.numeric(params$eGRID_year) %% 4 == 0 & as.numeric(params$eGRID_year) %% 100 != 0) | 
-        (as.numeric(params$eGRID_year) %% 400 == 0)) { 
-      feb_days <- 29
-    } else {feb_days <- 28}
-    df <- as.data.frame(cbind(year = params$eGRID_year, 
-                              month = c(rep(1, each = 31), 
-                                        rep(2, each = feb_days), 
-                                        rep(3, each = 31), 
-                                        rep(4, each = 30), 
-                                        rep(5, each = 31), 
-                                        rep(6, each = 30), 
-                                        rep(7, each = 31), 
-                                        rep(8, each = 31), 
-                                        rep(9, each = 30), 
-                                        rep(10, each = 31), 
-                                        rep(11, each = 30), 
-                                        rep(12, each = 31)), 
-                              day = c(1:31, 1:feb_days, 1:31, 1:30, 1:31, 1:30, 1:31, 1:31, 1:30, 1:31, 1:30, 1:31)))}
-  if (params$temporal_res == "hourly") { 
-    if ((as.numeric(params$eGRID_year) %% 4 == 0 & as.numeric(params$eGRID_year) %% 100 != 0) | 
-        (as.numeric(params$eGRID_year) %% 400 == 0)) { 
-      feb_days <- 29
-      days <- 366
-    } else {feb_days <- 28
-    days <- 365}
-    df <- as.data.frame(cbind(year = params$eGRID_year, 
-                              month = c(rep(1, each = 31), 
-                                        rep(2, each = feb_days), 
-                                        rep(3, each = 31), 
-                                        rep(4, each = 30), 
-                                        rep(5, each = 31), 
-                                        rep(6, each = 30), 
-                                        rep(7, each = 31), 
-                                        rep(8, each = 31), 
-                                        rep(9, each = 30), 
-                                        rep(10, each = 31), 
-                                        rep(11, each = 30), 
-                                        rep(12, each = 31)), 
-                              day = c(1:31, 1:feb_days, 1:31, 1:30, 1:31, 1:30, 1:31, 1:31, 1:30, 1:31, 1:30, 1:31),
-                              hour = rep(1:24, each = days)))}
-  
-  # Save temporal columns to add in params
-  params$temporal_res_cols_to_add <- df
-  
   return(params)
 }
 
