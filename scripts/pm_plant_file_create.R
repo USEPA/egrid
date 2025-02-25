6ec9031e34285fea96bc7672cea17a048e520c12
ABT\russelle <Emma.Russell@abtglobal.com>
2025-02-25 12:29:20 -0500

    Revising column naming and mutate process to format plant files

--- scripts/pm_plant_file_create.R
+++ scripts/pm_plant_file_create.R
@@ -19,7 +19,6 @@ library(readr)
 library(readxl)
 library(stringr)
 
-
 # Define eGRID year parameter ----------------
 # define parameter year if no one is currently assigned using prompted user input
 if (exists("params")) {
@@ -36,10 +35,6 @@ if (exists("params")) {
 }
 
 # Create function to format plant files ---------------
-emission_type <- "pm25"
-plant_data <- pm_plant_data
-unit_file <- pm_unit_file
-
 format_plant <- function(plant_data, unit_file, emission_type) {
   ## Assign emission sources to plant file -------
   # define source variable name
@@ -60,10 +55,12 @@ format_plant <- function(plant_data, unit_file, emission_type) {
   # adjust emissions for renewable fuel types and select desired columns
   plant_formatted <-
     plant_sources %>%
+    # replace emission with emission type in column names
+    rename_with(~gsub("emission", emission_type, .)) %>%
     # set annual emissions to NA for renewable fuel types
-    mutate("{emission_type}_ann" := if_else(emission_ann == 0 & primary_fuel_type %in% c("WAT", "SUN", "MWH", "WND", "WH", "PUR", "GEO", "NUC"), NA_real_, emission_ann),
+    mutate("{emission_type}_ann" := if_else(get(paste0(emission_type, "_ann")) == 0 & primary_fuel_type %in% c("WAT", "SUN", "MWH", "WND", "WH", "PUR", "GEO", "NUC"), NA_real_, get(paste0(emission_type, "_ann"))),
            # set output rate to 0 if annual net generation is less than 0
-           "{emission_type}_output_rate" := if_else(generation_ann < 0, 0, emission_output_rate)) %>%
+           "{emission_type}_output_rate" := if_else(generation_ann < 0, 0, get(paste0(emission_type, "_output_rate")))) %>%
     # select desired variables for final version
     select(plant_state, plant_name, plant_id, egrid_subregion, egrid_subregion_name, primary_fuel_type, nameplate_capacity, elec_allocation, generation_ann, heat_input, paste0(emission_type, "_ann"), paste0(emission_type, "_output_rate"), paste0(emission_type, "_input_rate"), paste0(emission_type, "_source"), unadj_combust_heat_input, paste0("unadj_", emission_type)) %>%
     # order by plant state abbreviation and plant name
@@ -72,13 +69,21 @@ format_plant <- function(plant_data, unit_file, emission_type) {
   return(plant_formatted)
 }
 
-# Load necessary data --------------------
-## PM2.5 unit file
+# Load unit files --------------------
 if(file.exists(glue::glue("data/outputs/{params$eGRID_year}/pm_unit_file.RDS"))) {
   pm_unit_file <- read_rds(glue::glue("data/outputs/{params$eGRID_year}/pm_unit_file.RDS")) #%>%
-    # rename(emission = glue::glue()
 } else {
-  stop("pm_unit_file.RDS does not exist. Run pm_unit_file_create.R to obtain.")}
+  stop("pm_unit_file.RDS does not exist. Run pm_nh3_voc_unit_file_create.R to obtain.")}
+
+if(file.exists(glue::glue("data/outputs/{params$eGRID_year}/nh3_unit_file.RDS"))) {
+  nh3_unit_file <- read_rds(glue::glue("data/outputs/{params$eGRID_year}/nh3_unit_file.RDS")) #%>%
+} else {
+  stop("nh3_unit_file.RDS does not exist. Run pm_nh3_voc_unit_file_create.R to obtain.")}
+
+if(file.exists(glue::glue("data/outputs/{params$eGRID_year}/voc_unit_file.RDS"))) {
+  voc_unit_file <- read_rds(glue::glue("data/outputs/{params$eGRID_year}/voc_unit_file.RDS")) #%>%
+} else {
+  stop("voc_unit_file.RDS does not exist. Run pm_nh3_voc_unit_file_create.R to obtain.")}
 
 # Run plant data creation script ---------
 source("scripts/functions/function_create_pm_nh3_voc_plant_data.R")
@@ -91,9 +96,8 @@ pm_plant_formatted <- format_plant(pm_plant_data, pm_unit_file, "pm25")
 nh3_plant_formatted <- format_plant(nh3_plant_data, nh3_unit_file, "nh3")
 voc_plant_formatted <- format_plant(voc_plant_data, voc_unit_file, "voc")
 
-# Export PM2.5 plant file -----
+# Export plant files -----
 source("scripts/functions/function_save_output_data.R")
 save_output_data(pm_plant_formatted, "pm_plant_file.RDS")
 save_output_data(nh3_plant_formatted, "nh3_plant_file.RDS")
 save_output_data(voc_plant_formatted, "voc_plant_file.RDS")
-
