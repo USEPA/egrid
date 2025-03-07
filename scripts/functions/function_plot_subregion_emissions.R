c92adf14567c4d6fce88b1fa10ab20e8f8abca49
ABT\russelle <Emma.Russell@abtglobal.com>
2025-03-07 17:10:01 -0500

    Creating script to produce subregion emission plots for pm, nh3, and voc values that match those previously in excel sheets

---
+++ scripts/functions/function_plot_subregion_emissions.R
@@ -0,0 +1,104 @@
+## -------------------------------
+## 
+## Plot subregion emissions
+##
+## Purpose: 
+## 
+## This function plots PM2.5, NH3, or VOC subregion emissions data
+## to be used in the final .xlsx sheet of data
+## 
+##
+## Additional notes
+##
+##      Emma Russell, Abt Global
+##
+## -------------------------------
+
+plot_subregion_emissions <- function(emission_type,
+                                     ydata,
+                                     fill_color,
+                                     ylabel,
+                                     annotate_label,
+                                     ylabel_min,
+                                     ylabel_max,
+                                     ylabel_int,
+                                     yaxis_max) {
+  
+  #' plot_subregion_emissions
+  #' 
+  #' Function to plot pm2.5, nh3, or voc subregion emissions data
+  #' 
+  #' @param emission_type Emission type to be calculated - either
+  #'                      "pm25", "nh3", or "voc"
+  #' @param ydata Data column name to be plotted on y axis - 
+  #'              must be a column name of subregion dataset
+  #' @param fill_color HEX code of bar plot fill color as character
+  #' @param ylabel Y-axis label for plot as character
+  #' @param annotate_label Plot title to be added as annotation as character
+  #' @param ylabel_min Lowest tick label on y-axis as numeric
+  #' @param ylabel_max Highest tick label on y-axis as numeric
+  #' @param ylabel_int Y-axis tick interval as numeric
+  #' @param yaxis_max Y-axis maximum value as numeric
+  #' 
+  #' @return Plot of desired emission type and data column
+  #'         
+  #' @examples 
+  #' # Create PM2.5 emissions plot
+  #' pm25_annual_generation <- plot_subregion_emissions(emission_type = "pm25",
+  #'                           ydata = "pm25_tons",
+  #'                           fill_color = "#FF0000",
+  #'                           ylabel = expression("PM"[2.5] ~ " Emissions (short tons)"),
+  #'                           annotate_label = expression(bold("PM"[2.5] ~ " Emissions")),
+  #'                           ylabel_min = 0,
+  #'                           ylabel_max = 8E4,
+  #'                           ylabel_int = 1E4,
+  #'                           yaxis_max = 8.65E4))
+  #' 
+  
+  # Require Libraries ---------
+  require(ggplot2)
+  require(readr)
+  require(scales)
+  
+  # Load subregion data -----
+  if(file.exists(glue::glue("data/outputs/{params$eGRID_year}/subregion_aggregation_{emission_type}.RDS"))) {
+    subregion_file <- read_rds(glue::glue("data/outputs/{params$eGRID_year}/subregion_aggregation_{emission_type}.RDS")) #%>%
+  } else {
+    stop(glue::glue("subregion_aggregation_{emission_type}.RDS does not exist. Run region_aggregation_create_pm_nh3_voc.R to obtain."))
+  }
+  
+  # Plot data -----
+  plot <- ggplot(subregion_file, aes(x = subregion, y = get(ydata))) +
+    geom_col(fill = fill_color, 
+             color = "black", 
+             width = 0.35, 
+             linewidth = 0.23) +
+    labs( x = "", 
+          y = ylabel) +
+    annotate("rect", 
+             xmin = 21.35, 
+             xmax = 27.1, 
+             ymin = yaxis_max - ((ylabel_max - ylabel_min) / 6), 
+             ymax = yaxis_max, 
+             alpha = 1, 
+             fill = "#E7E6E6", 
+             color = "black", 
+             linewidth = 0.23) +
+    annotate("text", 
+             x = 24.225, 
+             y = yaxis_max - ((ylabel_max - ylabel_min) / 12), 
+             label = annotate_label, 
+             size = 3, 
+             fontface = "bold") +
+    theme(panel.background = element_rect(fill = NA),
+          panel.grid.major.x = element_line(color = NA),
+          panel.grid.major.y = element_line(color = "#D9D9D9", size = 0.35, linetype = 1),
+          axis.ticks = element_line(linewidth = 0),
+          axis.text.x = element_text(angle = 48, vjust = 1.3, hjust=1, color = "#595959", size = 7.8),
+          axis.text.y = element_text(color = "#595959", size = 7.8),
+          axis.title.y = element_text(color = "#595959", size = 9, vjust = 1.3),
+          plot.margin=grid::unit(c(0.2,3.75,-3.6,6), "mm")) +
+    scale_y_continuous(limits = c(0, yaxis_max), breaks = seq(ylabel_min, ylabel_max, ylabel_int),  labels = label_comma())
+  
+  return(plot)
+}
---
+++ scripts/plot_subregion_emissions.R
@@ -0,0 +1,165 @@
+## -------------------------------
+##
+## Plot Subregion Emissions
+##
+## Purpose: 
+## 
+## This file plots PM2.5, NH3, and VOC subregion data using the function
+## function_plot_subregion_emissions.R 
+##
+##
+## Additional notes
+##
+##      Emma Russell, Abt Global
+##
+## -------------------------------
+
+# Load Libraries ---------
+library(ggplot2)
+library(readr)
+library(scales)
+
+# Define eGRID year parameter ----------------
+# define parameter year if no one is currently assigned using prompted user input
+if (exists("params")) {
+  if ("eGRID_year" %in% names(params)) { # if params() and params$eGRID_year exist, do not re-define
+    print("eGRID year parameter is already defined.")
+  } else { # if params() is defined, but eGRID_year is not, define it here
+    params$eGRID_year <- readline(prompt = "Input eGRID_year: ")
+    params$eGRID_year <- as.character(params$eGRID_year)
+  }
+} else { # if params() and eGRID_year are not defined, define them here
+  params <- list()
+  params$eGRID_year <- readline(prompt = "Input eGRID_year: ")
+  params$eGRID_year <- as.character(params$eGRID_year)
+}
+
+# Set function source
+source("scripts/functions/function_plot_subregion_emissions.R")
+
+# Set save directory and save function --------
+save_dir <- "data/static_tables/formatting/"
+
+save_fig <- function(plot_name) {
+  ggsave(paste0(save_dir, deparse(substitute(plot_name)), ".png"), plot = plot_name, width = 8.5, height = 2.89, units = "in")
+}
+
+# Plot PM2.5 -----
+## Annual Generation ------
+pm25_annual_generation <- plot_subregion_emissions(emission_type = "pm25",
+                                  ydata = "subregion_generation_ann",
+                                  fill_color = "#5B9BD5",
+                                  ylabel = "Annual Generation (MWh)",
+                                  annotate_label = "Generation",
+                                  ylabel_min = 0,
+                                  ylabel_max = 6E8,
+                                  ylabel_int = 1E8,
+                                  yaxis_max = 6.65E8)
+pm25_annual_generation
+save_fig(pm25_annual_generation)
+
+## Emissions -----
+pm25_emissions <- plot_subregion_emissions(emission_type = "pm25",
+                              ydata = "pm25_tons",
+                              fill_color = "#FF0000",
+                              ylabel = expression("PM"[2.5] ~ " Emissions (short tons)"),
+                              annotate_label = expression(bold("PM"[2.5] ~ " Emissions")),
+                              ylabel_min = 0,
+                              ylabel_max = 8E4,
+                              ylabel_int = 1E4,
+                              yaxis_max = 8.65E4)
+pm25_emissions
+save_fig(pm25_emissions)
+
+## Emission Rates -----
+pm25_rate <- plot_subregion_emissions(emission_type = "pm25",
+                         ydata = "pm25_rate",
+                         fill_color = "#70AD47",
+                         ylabel = expression("PM"[2.5] ~ " Emission Rates (lb/MWh)"),
+                         annotate_label = expression(bold("PM"[2.5] ~ " Emission Rates")),
+                         ylabel_min = 0,
+                         ylabel_max = 1.0,
+                         ylabel_int = 0.1,
+                         yaxis_max = 1.065)
+pm25_rate
+save_fig(pm25_rate)
+
+# Plot NH3 -----
+## Annual Generation ------
+nh3_annual_generation <- plot_subregion_emissions(emission_type = "nh3",
+                                   ydata = "subregion_generation_ann",
+                                   fill_color = "#5B9BD5",
+                                   ylabel = "Annual Generation (MWh)",
+                                   annotate_label = "Generation",
+                                   ylabel_min = 0,
+                                   ylabel_max = 6E8,
+                                   ylabel_int = 1E8,
+                                   yaxis_max = 6.65E8)
+nh3_annual_generation
+save_fig(nh3_annual_generation)
+
+## Emissions -----
+nh3_emissions <- plot_subregion_emissions(emission_type = "nh3",
+                           ydata = "nh3_tons",
+                           fill_color = "#70AD47",
+                           ylabel = expression("NH"[3] ~ " Emissions (short tons)"),
+                           annotate_label = expression(bold("NH"[3] ~ " Emissions")),
+                           ylabel_min = 0,
+                           ylabel_max = 1E4,
+                           ylabel_int = 1E3,
+                           yaxis_max = 1.065E4)
+nh3_emissions
+save_fig(nh3_emissions)
+
+## Emission Rates -----
+nh3_rate <- plot_subregion_emissions(emission_type = "nh3",
+                      ydata = "nh3_rate",
+                      fill_color = "#FF0000",
+                      ylabel = expression("NH"[3] ~ " Emission Rates (lb/MWh)"),
+                      annotate_label = expression(bold("NH"[3] ~ " Emission Rates")),
+                      ylabel_min = 0,
+                      ylabel_max = 0.3,
+                      ylabel_int = 0.05,
+                      yaxis_max = 0.3065)
+nh3_rate
+save_fig(nh3_rate)
+
+# Plot VOC -----
+## Annual Generation ------
+voc_annual_generation <- plot_subregion_emissions(emission_type = "voc",
+                                  ydata = "subregion_generation_ann",
+                                  fill_color = "#5B9BD5",
+                                  ylabel = "Annual Generation (MWh)",
+                                  annotate_label = "Generation",
+                                  ylabel_min = 0,
+                                  ylabel_max = 6E8,
+                                  ylabel_int = 1E8,
+                                  yaxis_max = 6.65E8)
+voc_annual_generation
+save_fig(voc_annual_generation)
+
+## Emissions -----
+voc_emissions <- plot_subregion_emissions(emission_type = "voc",
+                          ydata = "voc_tons",
+                          fill_color = "#70AD47",
+                          ylabel = "VOC Emissions (short tons)",
+                          annotate_label = "VOC Emissions",
+                          ylabel_min = 0,
+                          ylabel_max = 1E4,
+                          ylabel_int = 1E3,
+                          yaxis_max = 1.065E4)
+voc_emissions
+save_fig(voc_emissions)
+
+## Emission Rates -----
+voc_rate <- plot_subregion_emissions(emission_type = "voc",
+                     ydata = "voc_rate",
+                     fill_color = "#FF0000",
+                     ylabel = expression("VOC Emission Rates (lb/MWh)"),
+                     annotate_label = expression(bold("VOC Emission Rates")),
+                     ylabel_min = 0,
+                     ylabel_max = 0.45,
+                     ylabel_int = 0.05,
+                     yaxis_max = 0.4565)
+voc_rate
+save_fig(voc_rate)
