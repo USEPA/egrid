89ec48aac4c2ce9d21cf19f9ba28c9d68cd74ec2
ABT\gofortht <Teagan.Goforth@abtglobal.com>
2025-05-02 12:47:33 -0400

    Updating heat input source for manual change to plant 52152 unit 6RB

--- scripts/unit_file_create.R
+++ scripts/unit_file_create.R
@@ -794,10 +794,14 @@ all_units_2 <-
                                  NA_character_, .x))) %>% # set as NA to fill during next step
   group_by(month, plant_id) %>% 
   # Plant 52152 has positive heat input in two retired units. This heat input should be allocated to unit 6RB instead
-  mutate(heat_input = case_when(plant_id == "52152" & unit_id == "6RB" ~ sum(heat_input[plant_id == 52152], na.rm = TRUE), 
-                                plant_id == "52152" & !unit_id == "6RB" ~ NA_real_, 
-                                TRUE ~ heat_input)) %>% 
-  ungroup()
+  ### Note: check for updates or changes each data year ###
+  mutate(heat_input = case_when(plant_id == "52152" & unit_id == "6RB" ~ sum(heat_input[plant_id == "52152"], na.rm = TRUE), 
+                                plant_id == "52152" & !unit_id == "6RB" & primary_fuel_type == "BLQ" ~ NA_real_, 
+                                TRUE ~ heat_input),
+         heat_input_source = case_when(plant_id == "52152" & unit_id == "6RB" ~ "EIA Prime Mover-level Data", # set heat input source for plant 52152 unit 6RB
+                                       plant_id == "52152" & !unit_id == "6RB" ~ NA_character_, 
+                                       TRUE ~ heat_input_source)) %>% 
+  ungroup() 
 
 # identify units missing heat input values 
 
