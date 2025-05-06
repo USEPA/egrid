bd43b38b1b476bc84f82224cfbc8542b8d9c39db
ABT\russelle <Emma.Russell@abtglobal.com>
2025-05-06 15:05:00 -0400

    Creating loop to rename aggregation-level data to abbreviated names; adding data to new workbooks for each aggregation level

--- scripts/2b_pm_nh3_voc/final_formatting_pm_nh3_voc.R
+++ scripts/2b_pm_nh3_voc/final_formatting_pm_nh3_voc.R
@@ -13,9 +13,13 @@
 
 # Load libraries --------
 library(dplyr)
+library(openxlsx)
 library(readr)
 library(readxl)
 library(stringr)
+
+params <- list()
+params$eGRID_year <- "2022"
 
 # Define eGRID year parameter ----------------
 # define parameter year if no one is currently assigned using prompted user input
@@ -33,6 +37,11 @@ if (exists("params")) {
 }
 
 emission_type <- "pm25"
+if(emission_type == "pm25") {
+  emission_abbrev <- "pm"
+} else {
+  emission_abbrev <- emission_type
+}
 
 # Import .RDS data -----
 
@@ -46,37 +55,75 @@ filenames <- lapply(filename_types, function(filename) paste0(filename, paste0(e
 
 # import files in filenames list
 for (file in (filenames)){
+  if(grepl("_aggregation", file)) {
+    assign(str_replace(file, glue::glue("aggregation_{emission_type}.RDS"), "file"), read_rds(paste0(data_dir, file)))
+  } else {
   assign(str_remove(file,glue::glue("_{emission_type}.RDS")), read_rds(paste0(data_dir, file)))
+  }
 }
 
-# TABLE 1 - UNIT FILE -----
-
-## Set column titles and subtitles -----
-
-# Load abbreviated name to snake_case matches
+# Load abbreviated name to snake_case matches -----
 load("data/1_production_model/static_tables/name_matches.Rdata")
 
-# add additional column names present in unit data
-colnames_emissions <- setNames(c(paste0(emission_type, "_ann"), 
-                                 paste0(emission_type, "_rate"), 
-                                 paste0(emission_type, "_source")),
-                               c(paste0(toupper(emission_type), "AN"), 
-                                 paste0(toupper(emission_type), "RT"), 
-                                 paste0(toupper(emission_type), "SRC")))
+# Format emissions data for tables  -----
+level_abbrev <-c("unit" = "",
+                 "plant" = "PL",
+                 "state" = "ST",
+                 "subregion" = "SR")
 
-# select name matches present in unit data
-colnames_all <- c(unit_nonmetric[unit_nonmetric %in% colnames(unit_file)], colnames_emissions) %>%
-  print()
+for(emission_level in c("unit", "plant", "state", "subregion")) {
+  name_matches <- paste0(emission_level, "_nonmetric")
+  emission_data <- paste0(emission_level, "_file")
+  
+  # add additional column names present in unit data
+  colnames_emissions <- setNames(c(paste0(emission_type, "_ann"),
+                                   paste0(emission_type, "_output_rate"),
+                                   paste0(emission_type, "_input_rate"),
+                                   paste0(emission_type, "_rate"), 
+                                   paste0(emission_type, "_source"),
+                                   paste0("unadj_", emission_type)),
+                                 c(paste0(level_abbrev[[emission_level]], toupper(emission_type), "AN"), 
+                                   paste0(level_abbrev[[emission_level]], toupper(emission_type), "RTA"),
+                                   paste0(level_abbrev[[emission_level]], toupper(emission_type), "RA"),
+                                   paste0(level_abbrev[[emission_level]], toupper(emission_type), "RT"), 
+                                   paste0(toupper(emission_type), "SRC"),
+                                   paste0("UN", toupper(emission_type))))
 
-# rename unit column names
-unit_file_renamed <-
-  unit_file %>%
-  rename(!!!setNames(lapply(colnames_all, sym), names(colnames_all)))
+  # select name matches present in data
+  colnames <- c(get(name_matches)[get(name_matches) %in% colnames(get(emission_data))],
+                colnames_emissions[colnames_emissions %in% colnames(get(emission_data))])
+  
+  # rename emission data column names
+  emission_data_renamed <-
+    get(emission_data) %>%
+    rename(!!!setNames(lapply(colnames, sym), names(colnames)))
+  
+  # rename dataset variable
+  assign(paste0(emission_level, "_formatted"), emission_data_renamed) %>%
+    glimpse()
+}
 
-# write table data on sheets-----
+# Write Table Data ----
+wb <- createWorkbook()
 
-## Define names for title and subtitles -----
+for(emission_level in c("unit", "plant", "state", "subregion")) {
+  
+  # create new worksheet
+  current_worksheet <- glue::glue("{params$eGRID_year} {toupper(emission_abbrev)} {str_to_title(emission_level)}-level Data")
+  addWorksheet(wb, sheetName = current_worksheet)
+  
+  # write in data including titles
+  writeData(wb, sheet = current_worksheet, x = get(paste0(emission_level, "_formatted")), startCol = 1, startRow = 2)
+}
+  
+saveWorkbook(wb, glue::glue("data/2b_pm_nh3_voc/outputs/{params$eGRID_year}/{emission_type}_final.xlsx"), 
+             overwrite = TRUE)
 
-## write in the data
+# Format Table
 
-## Format as needed and desired
+## Headers
+## Subtitles
+## Colors
+## Font - size and BF
+## Cell sizes
+
