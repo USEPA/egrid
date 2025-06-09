ed482075f4067ee81ab6325f7f497c6484c1c9ed
ABT\russelle <Emma.Russell@abtglobal.com>
2025-06-09 16:39:01 -0400

    Creating QA script for emission source emissions comparison for VOC and NH3 emissions

---
+++ scripts/qa/access_R_qa/unit_summary_emission_source.R
@@ -0,0 +1,85 @@
+## -------------------------------
+##
+## Summary Statistics Emission Source
+## 
+## Purpose: 
+## 
+## This file pulls data from unit NH3 and VOC .RDS files to evaluate the differences across emission estimation sources  - used to evaluate the differences across estimation methods and determine the need for adjustment.
+##
+## Authors:  
+##      Emma Russell, Abt Global
+##
+## -------------------------------
+
+# Load libraries --------
+library(dplyr)
+library(janitor)
+library(tidyr)
+library(readr)
+
+# Define eGRID year parameter ----------------
+# define parameter year if no one is currently assigned using prompted user input
+if (exists("params")) {
+  if ("eGRID_year" %in% names(params)) { # if params() and params$eGRID_year exist, do not re-define
+    print("eGRID year parameter is already defined.")
+  } else { # if params() is defined, but eGRID_year is not, define it here
+    params$eGRID_year <- readline(prompt = "Input eGRID_year: ")
+    params$eGRID_year <- as.character(params$eGRID_year)
+  }
+} else { # if params() and eGRID_year are not defined, define them here
+  params <- list()
+  params$eGRID_year <- readline(prompt = "Input eGRID_year: ")
+  params$eGRID_year <- as.character(params$eGRID_year)
+}
+
+# Create save directory for QA outputs -----
+
+if(dir.exists("data/2b_pm_nh3_voc/outputs/qa")) {
+  print("Folder qa already exists.")
+}else{
+  dir.create("data/2b_pm_nh3_voc/outputs/qa")
+}
+
+if(dir.exists(glue::glue("data/2b_pm_nh3_voc/outputs/qa/{params$eGRID_year}"))) {
+  print(glue::glue("Folder qa/{params$eGRID_year} already exists."))
+}else{
+  dir.create(glue::glue("data/2b_pm_nh3_voc/outputs/qa/{params$eGRID_year}"))
+}
+
+# set directory for saving files 
+save_dir <- glue::glue("data/2b_pm_nh3_voc/outputs/qa/{params$eGRID_year}/")
+
+
+# Create function to summarize data -----
+unit_summary <- function(emission_type, year = params$eGRID_year) {
+  
+  # summarize unit emissions data by emission source type
+  source_summary <- read_rds(glue::glue("data/2b_pm_nh3_voc/outputs/{year}/unit_file_{emission_type}.RDS")) %>%
+    filter(!is.na(get(paste0(emission_type, "_source")))) %>%
+    group_by(get(paste0(emission_type, "_source"))) %>%
+    summarize(count = n(), 
+              emission_avg = mean(get(paste0(emission_type, "_ann")), na.rm = TRUE), 
+              rate_avg = mean(get(paste0(emission_type, "_rate")), na.rm = TRUE)) %>%
+    rename("{emission_type}_source" := `get(paste0(emission_type, "_source"))`)
+
+  primary_fuel_summary <-  read_rds(glue::glue("data/2b_pm_nh3_voc/outputs/{year}/unit_file_{emission_type}.RDS")) %>%
+    filter(!is.na(get(paste0(emission_type, "_source")))) %>%
+    group_by(get(paste0(emission_type, "_source")), primary_fuel_type) %>%
+    summarize(count = n(), 
+              emission_avg = mean(get(paste0(emission_type, "_ann")), na.rm = TRUE), 
+              rate_avg = mean(get(paste0(emission_type, "_rate")), na.rm = TRUE)) %>%
+    rename("{emission_type}_source" := `get(paste0(emission_type, "_source"))`)
+  
+  total_summary <- 
+    primary_fuel_summary %>%
+    bind_rows(source_summary) %>%
+    arrange(get(paste0(emission_type, "_source")), desc(is.na(primary_fuel_type))) %>%
+    mutate(primary_fuel_type = if_else(is.na(primary_fuel_type), "Total", primary_fuel_type))
+
+    # save output data in QA folder
+    write_csv(total_summary, glue::glue("{save_dir}{emission_type}_emissions_source_comparison_{year}.csv"))
+  }
+
+# Run and save summary statistics for different emission estimation sources -----
+unit_summary("nh3")
+unit_summary("voc")
