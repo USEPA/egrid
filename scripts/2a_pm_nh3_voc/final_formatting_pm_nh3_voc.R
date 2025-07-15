c2670d1e4acf7f327288c702b529e32dfd756e28
ABT\russelle <Emma.Russell@abtglobal.com>
2025-07-15 18:55:58 -0400

    Updating final figure production to produce new plots each time

--- scripts/2a_pm_nh3_voc/final_formatting_pm_nh3_voc.R
+++ scripts/2a_pm_nh3_voc/final_formatting_pm_nh3_voc.R
@@ -270,7 +270,7 @@ for (emission_type in c("pm25", "nh3", "voc")) {
   
   # run script to save subregion graphs
   source(glue::glue("scripts/functions/function_create_subregion_figures_pm_nh3_voc.R"))
-  create_subregion_emission_figures(emission_type, skip_if_exists = TRUE)
+  create_subregion_emission_figures(emission_type, skip_if_exists = FALSE)
   
   # define graph directory, names, and years
   graph_dir <- glue::glue("data/2a_pm_nh3_voc/static_tables/formatting/")
