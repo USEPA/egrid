5cf928e7804d987423c0d95c3d746979115b54d2
ABT\gofortht <Teagan.Goforth@abtglobal.com>
2025-08-07 09:13:43 -0400

    Updating file path to work in any working directory, adjusting renaming of files.

--- scripts/2c_api/api.R
+++ scripts/2c_api/api.R
@@ -1,12 +1,18 @@
 library(plumber)
+library(rstudioapi)
 library(readr)
 library(dplyr)
 
-base::load("data/1_production_model/static_tables/name_matches.RData")
+# Identify directory path egrid repository 
+file_path <- getSourceEditorContext()$path
+egrid_dir_path <- dirname(dirname(dirname(file_path)))
+
+# Load name matching 
+base::load(glue::glue("{egrid_dir_path}/data/1_production_model/static_tables/name_matches.RData"))
  
 #* @get /<year>/plant
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "plant_file.RDS") %>% 
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/plant_file.RDS") %>% 
     rename(any_of(plant_nonmetric)) %>% janitor::clean_names()
  
   tryCatch({
@@ -20,11 +26,12 @@ function(year) {
 
 #* @get /<year>/balancingauthority
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "ba_aggregation.RDS") %>% 
-    rename(any_of(ba_nonmetric)) %>% janitor::clean_names()
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/ba_aggregation.RDS") 
   
   tryCatch({
-    ba_data <- read_rds(rds_file)
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(ba_nonmetric)) %>% janitor::clean_names()
+
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
@@ -33,11 +40,12 @@ function(year) {
 
 #* @get /<year>/generator
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "generator_file.RDS") %>% 
-    rename(any_of(gen_nonmetric)) %>% janitor::clean_names()
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/generator_file.RDS") 
   
   tryCatch({
-    ba_data <- read_rds(rds_file)
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(gen_nonmetric)) %>% janitor::clean_names()
+
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
@@ -46,11 +54,12 @@ function(year) {
 
 #* @get /<year>/nerc
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "nerc_aggregation.RDS") %>% 
-    rename(any_of(nerc_nonmetric)) %>% janitor::clean_names()
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/nerc_aggregation.RDS") 
   
   tryCatch({
-    ba_data <- read_rds(rds_file)
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(nerc_nonmetric)) %>% janitor::clean_names()
+    
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
@@ -59,11 +68,12 @@ function(year) {
 
 #* @get /<year>/state
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "state_aggregation.RDS") %>% 
-    rename(any_of(state_nonmetric)) %>% janitor::clean_names()
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/state_aggregation.RDS") 
   
   tryCatch({
-    ba_data <- read_rds(rds_file)
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(state_nonmetric)) %>% janitor::clean_names()
+
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
@@ -72,11 +82,12 @@ function(year) {
 
 #* @get /<year>/subregion
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "subregion_file.RDS") %>% 
-    rename(any_of(subregion_nonmetric)) %>% janitor::clean_names()
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/subregion_file.RDS") 
   
   tryCatch({
-    ba_data <- read_rds(rds_file)
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(subregion_nonmetric)) %>% janitor::clean_names()
+
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
@@ -86,10 +97,12 @@ function(year) {
 
 #* @get /<year>/unit
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "unit_file.RDS")
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/unit_file.RDS")
   
   tryCatch({
-    ba_data <- read_rds(rds_file)
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(unit_nonmetric)) %>% janitor::clean_names()
+    
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
@@ -98,11 +111,12 @@ function(year) {
 
 #* @get /<year>/us
 function(year) {
-  rds_file <- file.path("data", "1_production_model", "outputs", year, "us_aggregation.RDS") %>% 
-    rename(any_of(us_nonmetric)) %>% janitor::clean_names()
+  rds_file <- glue::glue("{egrid_dir_path}/data/1_production_model/outputs/{year}/us_aggregation.RDS") 
   
   tryCatch({
-    ba_data <- read_rds(rds_file) 
+    ba_data <- read_rds(rds_file) %>% 
+      rename(any_of(us_nonmetric)) %>% janitor::clean_names()
+
     list(success = TRUE, data = ba_data)
   }, error = function(e) {
     list(success = FALSE, error = e$message)
--- scripts/2c_api/run_api.R
+++ scripts/2c_api/run_api.R
@@ -1,5 +1,10 @@
 library(plumber)
+library(rstudioapi)
+
+# Identify directory path for api.R
+file_path <- getSourceEditorContext()$path
+dir_path <- sub("run_api.R", "", file_path)
 
 # Load and run the Plumber API
-pr <- plumber::plumb("api.R") # Load the route definitions from api.R
+pr <- plumber::plumb("api.R", dir = dir_path) # Load the route definitions from api.R
 pr$run(port = 8001)           # Specify the port
