6c856ef0653a826d1dc5321fa5ce6e6e2306e251
ABT\gofortht <Teagan.Goforth@abtglobal.com>
2025-09-04 12:28:04 -0400

    Adding year to epa_plants_to_delete table and adding filter in unit file to only include years that are less than or equal to the eGRID year running.

--- scripts/1_production_model/unit_file_create.R
+++ scripts/1_production_model/unit_file_create.R
@@ -156,7 +156,10 @@ if (bio_units_to_add_flag) {
 ### Note: check for updates or changes each data year ###
 epa_plants_to_delete <- read_csv("data/1_production_model/static_tables/epa_plants_to_delete.csv", 
                                   col_types = "c") %>% 
-                        select("plant_id" = "ORIS Code") 
+                        janitor::clean_names() %>% 
+                        select("year",
+                               "plant_id" = "oris_code") %>% 
+                        filter(year <= params$eGRID_year)
 
 # Emission factors 
 # physical units emission factors
